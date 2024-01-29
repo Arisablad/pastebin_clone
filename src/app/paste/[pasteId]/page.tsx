a5bfd87efda0e5c5954249207492faf634c3adee
Remigiusz Szostak <145459815+Arisablad@users.noreply.github.com>
2024-01-29 18:15:55 +0100

    added basic ui for paste comments

--- src/app/paste/[pasteId]/page.tsx
+++ src/app/paste/[pasteId]/page.tsx
@@ -111,6 +111,63 @@ function SinglePastePage() {
               <span className="text-blue-400">Syntax : </span> {paste.syntax}
             </li>
           </ul>
+
+
+          {/* COMMENTS */}
+          <div className='rounded-md mt-8 lg:col-span-8 flex flex-col py-2'>
+            <h1 className='text-2xl text-center py-4'>Comments</h1>
+            {/* COMMENTS LIST */}
+            <div className='flex flex-col px-4 gap-2'>
+              <div className='bg-secondary flex flex-col rounded-md px-2 py-4 shadow-md'>
+                <div className=' flex gap-4 py-2'>
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user"><circle cx="12" cy="12" r="10" /><circle cx="12" cy="10" r="3" /><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" /></svg>
+                  </div>
+                  <p>Arek Zielarek</p>
+                </div>
+                <div className=''>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, mollitia labore cum soluta fugiat quaerat rem tenetur.
+                  Quam esse aliquid itaque, magni deleniti enim omnis culpa voluptatum quidem aut quos quod, debitis dolorum explicabo? Aut.
+                </div>
+              </div>
+
+              <div className='bg-secondary flex flex-col rounded-md px-2 py-4 shadow-md'>
+                <div className=' flex gap-4 py-2'>
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user"><circle cx="12" cy="12" r="10" /><circle cx="12" cy="10" r="3" /><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" /></svg>
+                  </div>
+                  <p>Arek Zielarek</p>
+                </div>
+                <div className=''>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, mollitia labore cum soluta fugiat quaerat rem tenetur.
+                  Quam esse aliquid itaque, magni deleniti enim omnis culpa voluptatum quidem aut quos quod, debitis dolorum explicabo? Aut.
+                </div>
+              </div>
+
+              <div className='bg-secondary flex flex-col rounded-md px-2 py-4 shadow-md'>
+                <div className=' flex gap-4 py-2'>
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user"><circle cx="12" cy="12" r="10" /><circle cx="12" cy="10" r="3" /><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" /></svg>
+                  </div>
+                  <p>Arek Zielarek</p>
+                </div>
+                <div className=''>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, mollitia labore cum soluta fugiat quaerat rem tenetur.
+                  Quam esse aliquid itaque, magni deleniti enim omnis culpa voluptatum quidem aut quos quod, debitis dolorum explicabo? Aut.
+                </div>
+              </div>
+
+            </div>
+
+
+            {/*WRITE YOUR COMMENT HERE*/}
+            <div className='flex items-center gap-4 mt-12'>
+              <textarea className='w-full p-4' placeholder='write your comment here'></textarea>
+            </div>
+          </div>
+
+
+
         </div>
       ) : loading ? (
         <div className="w-full lg:col-span-8">

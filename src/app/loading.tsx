70500dbe94e3bc88da0d2b38297d7bafef401527
Remigiusz Szostak <145459815+Arisablad@users.noreply.github.com>
2024-01-30 12:55:09 +0100

    Create loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+const Loading = () => {
+
+
+    return <div className="flex flex-col gap-4">
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+        <Skeleton className="h-12 w-full rounded-full bg-accent-foreground" />
+    </div>
+}
+
+
+
+export default Loading
